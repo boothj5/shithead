a0c996b3635b9784eb196c424c80e0a2c4831cfb
James Booth <jamesbooth2000@hotmail.com>
2011-01-22 17:03:43 +0000

    Deal tests

--- ShitheadGameTest.java
+++ ShitheadGameTest.java
@@ -23,5 +23,53 @@ public class ShitheadGameTest {
 		assertThat("Correct cards per hand", details.getNumCardsPerHand(), is(numCardsPerHand)) ;
 		
 	}
+	
+	@Test
+	public void dealResultsIn52CardsInGame() {
+		int numPlayers = 3 ;
+		int numCardsPerHand = 4 ;
+		
+		List<String> playerNames = new ArrayList<String>() ;
+		playerNames.add("James") ;
+		playerNames.add("Bob") ;
+		playerNames.add("Monkey") ;
+		
+		ShitheadGame game = new ShitheadGame(numPlayers, playerNames, numCardsPerHand) ;
+		game.deal();
+		ShitheadGameDetails details = game.getGameDetails() ;
+		
+		int totalCardsInGame = details.getDeck().getSize() + 
+								details.getPile().size() + 
+								details.getBurnt().size() ;
+		
+		int playerCards = 0 ;
+		for (Player player : details.getPlayers()) {
+			playerCards += player.getHand().size() ;
+			playerCards += player.getFaceUp().size() ;
+			playerCards += player.getFaceDown().size() ;
+		}
+		
+		
+		totalCardsInGame += playerCards ;
+		
+		assertThat(totalCardsInGame, is(52)) ;
+	}
 
+	@Test
+	public void dealLeavesCorrectAmountInDeck() {
+		int numPlayers = 3 ;
+		int numCardsPerHand = 4 ;
+		
+		List<String> playerNames = new ArrayList<String>() ;
+		playerNames.add("James") ;
+		playerNames.add("Bob") ;
+		playerNames.add("Monkey") ;
+		
+		ShitheadGame game = new ShitheadGame(numPlayers, playerNames, numCardsPerHand) ;
+		game.deal();
+		ShitheadGameDetails details = game.getGameDetails() ;
+		int numberOfCardsThatShouldBeLeft = 52 - ((numPlayers * numCardsPerHand) * 3) ;
+		
+		assertThat(details.getDeck().getSize(), is(numberOfCardsThatShouldBeLeft)) ;
+	}
 }
