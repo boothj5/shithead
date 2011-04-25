4aedbf174e984188fd76da0871365a71b6d3dbf6
James Booth <jamesbooth2000@hotmail.com>
2011-04-25 21:44:13 +0100

    Popup menu

--- src/com/boothj5/shithead/gui/ShitheadGUI.java
+++ src/com/boothj5/shithead/gui/ShitheadGUI.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -14,6 +16,7 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
 import javax.swing.border.EtchedBorder;
@@ -23,14 +26,84 @@ public class ShitheadGUI extends JFrame {
 	private static final long serialVersionUID = 1L;
 	
 	private JLabel statusbar ;
+	private JPopupMenu popupmenu ;
 
 	public ShitheadGUI() {
        initGUI();
     }
 
 	private void initGUI() {
-        JMenuBar menu = new JMenuBar();
+        JPanel panel = createPanel();
+        getContentPane().add(panel);
 
+        JMenuBar menu = createMainMenu();
+        setJMenuBar(menu);
+        createStatusBar();
+
+        JButton quitButton = createQuitButton();	    
+        panel.add(quitButton);
+
+        createPopupmenu();        
+        
+        setTitle("Shithead GUI");
+        setSize(800, 800);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+    private void createPopupmenu() {
+        popupmenu = new JPopupMenu();
+        JMenuItem menuItemQuit = new JMenuItem("Quit");         
+        JMenuItem menuItemHelp = new JMenuItem("Help");
+        menuItemQuit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        popupmenu.add(menuItemHelp);
+        popupmenu.add(menuItemQuit);
+
+        this.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                if (e.getButton() == MouseEvent.BUTTON3) {
+                    popupmenu.show(e.getComponent(), e.getX(), e.getY());
+                }
+            }
+        });
+    }
+
+    private JPanel createPanel() {
+        JPanel panel = new JPanel();
+        panel.setLayout(null);
+        return panel;
+    }
+
+    private JButton createQuitButton() {
+        JButton quitButton = new JButton("Quit");
+        quitButton.setBounds(50, 60, 80, 30);
+        quitButton.setToolTipText("Press to quit") ;
+        quitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                System.exit(0);
+            }
+        });
+        return quitButton;
+    }
+
+    private void createStatusBar() {
+        statusbar = new JLabel(" Statusbar");
+        statusbar.setBorder(BorderFactory.createEtchedBorder(
+                EtchedBorder.RAISED));
+        add(statusbar, BorderLayout.SOUTH);
+    }
+
+    private JMenuBar createMainMenu() {
+        JMenuBar menu = new JMenuBar();    
+        
         JMenu file = new JMenu("File");
         file.setMnemonic(KeyEvent.VK_F);
 
@@ -39,7 +112,6 @@ public class ShitheadGUI extends JFrame {
 
         JCheckBoxMenuItem showbar = new JCheckBoxMenuItem("StatusBar");
         showbar.setState(true);
-
         showbar.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent event) {
               if (statusbar.isVisible()) {
@@ -86,35 +158,8 @@ public class ShitheadGUI extends JFrame {
         
         menu.add(file);
         menu.add(view);
-        
-        setJMenuBar(menu);
-        
-        statusbar = new JLabel(" Statusbar");
-        statusbar.setBorder(BorderFactory.createEtchedBorder(
-                EtchedBorder.RAISED));
-        add(statusbar, BorderLayout.SOUTH);
-        
-        JPanel panel = new JPanel();
-        getContentPane().add(panel);
-        panel.setLayout(null);
-
-        JButton quitButton = new JButton("Quit");
-        quitButton.setBounds(50, 60, 80, 30);
-        quitButton.setToolTipText("Press to quit") ;
-        quitButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent event) {
-                System.exit(0);
-            }
-        });	    
-
-        panel.add(quitButton);
-        
-        setTitle("Shithead GUI");
-        setSize(800, 800);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-	}
+        return menu;
+    }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
