beb90014278a9901a47e0de167982f69a4cc6086
James Booth <jamesbooth2000@hotmail.com>
2011-04-25 21:09:28 +0100

    Added statusbar

--- src/com/boothj5/shithead/gui/ShitheadGUI.java
+++ src/com/boothj5/shithead/gui/ShitheadGUI.java
@@ -1,21 +1,28 @@
 package com.boothj5.shithead.gui;
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
+import javax.swing.border.EtchedBorder;
 
 public class ShitheadGUI extends JFrame {
 
 	private static final long serialVersionUID = 1L;
+	
+	private JLabel statusbar ;
 
 	public ShitheadGUI() {
        initGUI();
@@ -27,6 +34,23 @@ public class ShitheadGUI extends JFrame {
         JMenu file = new JMenu("File");
         file.setMnemonic(KeyEvent.VK_F);
 
+        JMenu view = new JMenu("View");
+        view.setMnemonic(KeyEvent.VK_V);
+
+        JCheckBoxMenuItem showbar = new JCheckBoxMenuItem("StatusBar");
+        showbar.setState(true);
+
+        showbar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+              if (statusbar.isVisible()) {
+                  statusbar.setVisible(false);
+              } else {
+                  statusbar.setVisible(true);
+              }
+            }
+
+        });        
+        
         JMenuItem exitShithead = new JMenuItem("Quit") ;
         exitShithead.setMnemonic(KeyEvent.VK_X);
         exitShithead.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X,
@@ -58,9 +82,17 @@ public class ShitheadGUI extends JFrame {
         file.addSeparator();
         file.add(exitShithead);
         
+        view.add(showbar);
+        
         menu.add(file);
+        menu.add(view);
         
         setJMenuBar(menu);
+        
+        statusbar = new JLabel(" Statusbar");
+        statusbar.setBorder(BorderFactory.createEtchedBorder(
+                EtchedBorder.RAISED));
+        add(statusbar, BorderLayout.SOUTH);
         
         JPanel panel = new JPanel();
         getContentPane().add(panel);
