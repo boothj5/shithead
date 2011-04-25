8322d236de5a5594c769935d2073dc120167cc3c
James Booth <jamesbooth2000@hotmail.com>
2011-04-25 20:48:08 +0100

    Main menus

--- src/com/boothj5/shithead/gui/ShitheadGUI.java
+++ src/com/boothj5/shithead/gui/ShitheadGUI.java
@@ -2,9 +2,13 @@ package com.boothj5.shithead.gui;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
@@ -17,6 +21,39 @@ public class ShitheadGUI extends JFrame {
     }
 
 	private void initGUI() {
+        JMenuBar menu = new JMenuBar();
+
+        JMenu file = new JMenu("File");
+        file.setMnemonic(KeyEvent.VK_F);
+
+        JMenuItem exitShithead = new JMenuItem("Quit") ;
+        exitShithead.setMnemonic(KeyEvent.VK_X);
+        exitShithead.setToolTipText("Exit Shithead application");
+        exitShithead.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+                
+            }
+        });
+        
+        JMenu game = new JMenu("Game");
+        game.setMnemonic(KeyEvent.VK_G);
+
+        JMenuItem newGame = new JMenuItem("New") ;
+        JMenuItem gameTime = new JMenuItem("Show game time");
+
+        game.add(newGame);
+        game.add(gameTime);
+        
+        file.add(game);
+        file.addSeparator();
+        file.add(exitShithead);
+        
+        menu.add(file);
+        
+        setJMenuBar(menu);
+        
         JPanel panel = new JPanel();
         getContentPane().add(panel);
         panel.setLayout(null);
@@ -25,6 +62,7 @@ public class ShitheadGUI extends JFrame {
         quitButton.setBounds(50, 60, 80, 30);
         quitButton.setToolTipText("Press to quit") ;
         quitButton.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent event) {
                 System.exit(0);
             }
