d0c793dc88fccbcdf1a72161ee3ab1b7927f4c10
James Booth <jamesbooth2000@hotmail.com>
2011-06-01 23:10:38 +0100

    Better form display

--- src/com/boothj5/shithead/gui/DetailsPane.java
+++ src/com/boothj5/shithead/gui/DetailsPane.java
@@ -1,11 +1,13 @@
 package com.boothj5.shithead.gui;
 
+import javax.swing.BorderFactory;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
 import javax.swing.JFrame;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JLabel;
+import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
 
 import java.awt.BorderLayout;
@@ -34,7 +36,7 @@ import java.util.Set;
 public class DetailsPane extends JPanel implements ActionListener {
     private JTextField playerName;
     private JComboBox compType;
-    private JLabel compDescription ;
+    private JTextArea compDescription ;
     private String[] compTypesChoice = {};
 
     /**
@@ -46,7 +48,7 @@ public class DetailsPane extends JPanel implements ActionListener {
         JLabel welcomeLabel = new JLabel("Welcome to Javahead!");
         welcomeLabel.setFont(new Font("Dialog", Font.BOLD, 22));
         
-        JLabel playerNameLabel = new JLabel("Player's name: ");
+        JLabel playerNameLabel = new JLabel("Player name: ");
         playerName = new JTextField();
         playerName.setColumns(10);
         
@@ -60,9 +62,16 @@ public class DetailsPane extends JPanel implements ActionListener {
         compType.setActionCommand("selectComp") ;
         compType.addActionListener(this) ;
 
-        compDescription = new JLabel(PlayerFactory.computerPlayerDescriptions().get(compType.getSelectedItem())) ;
+        JLabel compTacticsLabel = new JLabel("AI tactics: ") ;
+        compDescription = new JTextArea(PlayerFactory.computerPlayerDescriptions().get(compType.getSelectedItem())) ;
+        compDescription.setColumns(20) ;
+        compDescription.setRows(5) ;
+        compDescription.setLineWrap(true) ;
+        compDescription.setWrapStyleWord(true) ;
+        compDescription.setEditable(false) ;
         
         JPanel entryPanel = new JPanel(new GridBagLayout()) ;
+        entryPanel.setBorder(BorderFactory.createTitledBorder("Enter game details")) ;
         
         GridBagConstraints labelConstraints = new GridBagConstraints() ;
         labelConstraints.gridx = 0 ;
@@ -73,6 +82,8 @@ public class DetailsPane extends JPanel implements ActionListener {
         entryPanel.add(playerNameLabel, labelConstraints) ;
         labelConstraints.gridy = 1 ;
         entryPanel.add(computerLabel, labelConstraints) ;
+        labelConstraints.gridy = 2 ;
+        entryPanel.add(compTacticsLabel, labelConstraints) ;
         
         GridBagConstraints fieldConstraints = new GridBagConstraints() ;
         fieldConstraints.gridx = 1 ;
@@ -83,6 +94,8 @@ public class DetailsPane extends JPanel implements ActionListener {
         entryPanel.add(playerName, fieldConstraints) ;
         fieldConstraints.gridy = 1 ;
         entryPanel.add(compType, fieldConstraints) ;
+        fieldConstraints.gridy = 2 ;
+        entryPanel.add(compDescription, fieldConstraints) ;
         
         // deal (start) button
         JButton dealButton = new JButton("Deal");
@@ -104,24 +117,18 @@ public class DetailsPane extends JPanel implements ActionListener {
         GridBagConstraints pc = new GridBagConstraints() ;
         pc.weightx = 1.0 ; 
         pc.weighty = 1.0 ; 
+        pc.fill = GridBagConstraints.BOTH ;
         pc.gridx = 0 ;
         pc.gridy = 1 ;
         pc.anchor = GridBagConstraints.PAGE_START ;
         add(entryPanel, pc) ;
         
-        GridBagConstraints cdc = new GridBagConstraints() ;
-        cdc.weightx = 1.0 ; 
-        cdc.weighty = 1.0 ; 
-        cdc.gridx = 0 ;
-        cdc.gridy = 2 ;
-        cdc.anchor = GridBagConstraints.PAGE_START ;
-        add(compDescription, cdc) ;
         
         GridBagConstraints dc = new GridBagConstraints() ;
         dc.weightx = 1.0 ; 
         dc.weighty = 1.0 ; 
         dc.gridx = 0 ;
-        dc.gridy = 3 ;
+        dc.gridy = 2 ;
         dc.anchor = GridBagConstraints.PAGE_START ;
         add(dealButton, dc) ;
 
